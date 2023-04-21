3179cd374ede9dc17759938405fc284222330b99
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2023-04-21 15:49:57 +0430

    person and student api tested

--- amuzeshyar/apis.py
+++ amuzeshyar/apis.py
@@ -2,180 +2,166 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import (
-    PersonSerializer,
-    EmailSerializer,
-    PhoneNumberSerializer,
-    StudentResponseSerializer, 
-    StudentRequestSerializer, 
-    AddressSerializer)
+from django.core.exceptions import ObjectDoesNotExist
+
+from amuzeshyar import serializers as ser
 from .models import Person as PersonModel
 from .models import Student as StudentModel
 
 
-
-# create your api here
-# https://www.django-rest-framework.org/tutorial/quickstart/
-
-class Person(APIView):    
+class Person(APIView):
     def post(self, request):
         data = request.data
-        if data: 
-            serialized = PersonSerializer(data=data)
+        if data:
+            serialized = ser.PersonRequestSerializer(data=data)
             if serialized.is_valid():
                 serialized.save()
                 return Response(serialized.data, status.HTTP_201_CREATED)
-            return Response({"erorr":"data is unvalid",'details':serialized.errors}, status.HTTP_400_BAD_REQUEST )
-        return Response({"erorr":"empty payload"},status.HTTP_400_BAD_REQUEST )
+            return Response({"error": "data is invalid", 'details': serialized.errors}, status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "empty payload"}, status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, national_id):
-        queryset = PersonModel.objects.get(national_id =national_id )
-        serialized = PersonSerializer(queryset, many=True)
+        try:
+            queryset = PersonModel.objects.get(national_id=national_id)
+        except ObjectDoesNotExist:
+            return Response({"msg": "object not found"}, status=status.HTTP_404_NOT_FOUND)
+        serialized = ser.PersonResponseSerializer(queryset)
         return Response(serialized.data, status.HTTP_200_OK)
 
     def delete(self, request, national_id):
-        qs = PersonModel.objects.filter(national_id = national_id).first()
-        if qs: 
+        qs = PersonModel.objects.filter(national_id=national_id).first()
+        if qs:
             qs.delete()
-            return Response({"msg":"deleted"}, status.HTTP_200_OK)
-        return Response({"msg":"not found"},status.HTTP_404_NOT_FOUND)
+            return Response({"msg": "deleted"}, status.HTTP_200_OK)
+        return Response({"msg": "not found"}, status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, national_id):
+        pass
+
+    def patch(self, request, national_id):
+        pass
+
+    def delete(self, request, national_id):
+        pass
+
+    @api_view(['GET'])
+    def all_persons(request):
+        persons_qs = PersonModel.objects.all()
+        serialized_data = ser.PersonResponseSerializer(
+            persons_qs, many=True).data
+        return Response(serialized_data, status.HTTP_200_OK)
 
 
-# @api_view(['DELETE'])
-# def del_person(self, request, national_id):
-#     qs = PersonModel.objects.filter(national_id = national_id).first()
-#     if qs: 
-#         serialized = PersonSerializer(instance=qs)
-#         return Response(serialized.data, status.HTTP_200_OK)
-#     return Response({"msg":"not found"},status.HTTP_404_NOT_FOUND)
-
-@api_view(['POST'])
-def register_student(request):
-    
-    email_address = request.data.get('email')
-    phone_number = request.data.get('phone_number')
-    person_data = request.data
-    
-    serialized = PersonSerializer(data=person_data)
-    if serialized.is_valid():
-        serialized.save()
-        person_id = serialized.data.get("national_id")
-
-    # handling email 
-    email_data = {
-        "email": email_address,
-        "person": person_id
-    }
-    
-    email_serialized = EmailSerializer(data=email_data)
-    if email_serialized.is_valid():
-        email_serialized.save()
-    
-    # handling phonenumber 
-    phone_data = {
-        "number": phone_number,
-        "person": person_id
-    }
-    
-    phone_serialized = PhoneNumberSerializer(data=phone_data)
-    if phone_serialized.is_valid():
-        phone_serialized.save()
-    
-    return Response({"msg": "Success"}, status=status.HTTP_201_CREATED)
-    
 class Student(APIView):
     def get(self, request, *args, **kwargs):
         student_id = kwargs.get('student_id', None)
-        if student_id: 
+        if student_id:
             student = StudentModel.objects.filter(id=student_id).first()
-            if student: 
-                serialized_data = StudentResponseSerializer(instance=student).data
+            if student:
+                serialized_data = ser.StudentResponseSerializer(
+                    instance=student).data
                 return Response(
                     {
-                    "msg":"success",
-                    "data":serialized_data
+                        "msg": "success",
+                        "data": serialized_data
                     },
                     status=status.HTTP_200_OK
-                    )
-            return Response({"msg":"student not found"},status=status.HTTP_404_NOT_FOUND)
-        return Response({"msg":"student id is required"},status=status.HTTP_400_BAD_REQUEST)
-            
+                )
+            return Response({"msg": "student not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"msg": "student id is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-    
     def post(self, request, *args, **kwargs):
         payload = request.data
         national_id = payload.get('national_id')
         if payload and national_id:
             emails = request.data.get('emails', [])
-            phone_numbers = request.data.get('phone_numbers',[])
-            addresses = request.data.get('addresses',[])
-            
+            phone_numbers = request.data.get('phone_numbers', [])
+            addresses = request.data.get('addresses', [])
+
             # making emails, phone numbers, addresses objects
-            emails_data = [{"email": email, "person":national_id} for email in emails]
-            phone_numbers_data = [{"number": phone, "person": national_id} for phone in phone_numbers]
-            addresses_data = [{"address": address, "person": national_id} for address in addresses]
-            
-            person_ser = PersonSerializer(data=payload)
-            student_ser = StudentRequestSerializer(data=payload)
-            email_ser = EmailSerializer(data=emails_data, many=True)
-            phone_number_ser = PhoneNumberSerializer(data=phone_numbers_data, many=True)
-            address_ser = AddressSerializer(data=addresses_data, many=True)
-            
+            emails_data = [{"email": email, "person": national_id}
+                           for email in emails]
+            phone_numbers_data = [
+                {"number": phone, "person": national_id} for phone in phone_numbers]
+            addresses_data = [{"address": address,
+                               "person": national_id} for address in addresses]
+
+            person_ser = ser.PersonRequestSerializer(data=payload)
+            if person_ser.is_valid():
+                person_ser.save()
+                payload['person'] = national_id
+            else:
+                return Response(
+                    {
+                        "msg": "failure",
+                        "detail": person_ser.errors
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            student_ser = ser.StudentRequestSerializer(data=payload)
+            email_ser = ser.EmailSerializer(data=emails_data, many=True)
+            phone_number_ser = ser.PhoneNumberSerializer(
+                data=phone_numbers_data, many=True)
+            address_ser = ser.AddressSerializer(data=addresses_data, many=True)
+
             is_all_valid = all([
-                person_ser.is_valid(),
                 student_ser.is_valid(),
                 phone_number_ser.is_valid(),
                 address_ser.is_valid(),
                 email_ser.is_valid()
             ])
             if is_all_valid:
-                person_ser.save()
                 student_ser.save()
                 phone_number_ser.save()
                 address_ser.save()
                 email_ser.save()
-                
+
                 # preparing student data for response display
-                student = StudentModel.objects.filter(student_id=student_ser.student_id).first()
-                student_data = StudentResponseSerializer(student).data
+                student = StudentModel.objects.filter(
+                    id=student_ser.data.get('id')).first()
+                student_data = ser.StudentResponseSerializer(student).data
                 return Response(
                     {
-                    "msg":"success",
-                    "data": student_data
+                        "msg": "success",
+                        "data": student_data
                     },
                     status=status.HTTP_201_CREATED
                 )
-            else: 
-                all_errors = []
-                if person_ser.errors: all_errors.append(person_ser.errors)
-                if student_ser.errors: all_errors.append(student_ser.errors)
-                if phone_number_ser.errors: all_errors.append(phone_number_ser.errors)
-                if address_ser.errors: all_errors.append(address_ser.errors)
-                if email_ser.errors: all_errors.append(email_ser.errors)
+            else:
+
+                PersonModel.objects.get(national_id=national_id).delete()
+                all_errors = {
+                    "StudentInfo": student_ser.errors,
+                    "Emails": email_ser.errors,
+                    "PhoneNumbers": phone_number_ser.errors,
+                    "Addresses": address_ser.errors,
+                }
+
                 return Response(
                     {
-                    "msg":"failure",
-                    "detail": all_errors
+                        "msg": "failure",
+                        "detail": all_errors
                     },
                     status=status.HTTP_400_BAD_REQUEST
                 )
-                      
+
         return Response(
             {
-            "msg":"failure",
-            "detail": "payload is required"
+                "msg": "failure",
+                "detail": "payload is required"
             },
             status=status.HTTP_400_BAD_REQUEST)
-    
+
     def put(self, request, *args, **kwargs):
         pass
-    
+
     def patch(self, request, *args, **kwargs):
         pass
-    
+
     def delete(self, request, *args, **kwargs):
         pass
-    
+
     @api_view(['GET'])
     def get_students(request, *args, **kwargs):
         pass
--- amuzeshyar/serializers.py
+++ amuzeshyar/serializers.py
@@ -4,25 +4,29 @@ from .models import (
     Person,
     Email,
     PhoneNumber,
-    Student, 
+    Student,
     Address,
-    )
+)
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonResponseSerializer(serializers.ModelSerializer):
     role = serializers.SerializerMethodField()
     gender = serializers.SerializerMethodField()
+
     class Meta:
         model = Person
         fields = "__all__"
 
     def get_role(self, obj):
-        return obj.role.title
-
+        return obj.role.title if obj.role else None
+    
     def get_gender(self, obj):
-        if obj.gender:
-            return "مرد"
-        return "زن"
+        return "مرد" if obj.gender else "زن"
+
+class PersonRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = "__all__"
 class EmailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Email
@@ -36,54 +40,47 @@ class PhoneNumberSerializer(serializers.ModelSerializer):
 
 
 class StudentResponseSerializer(serializers.ModelSerializer):
-    person = PersonSerializer()
+    person = PersonResponseSerializer()
     registration_type = serializers.SerializerMethodField()
     field_of_study = serializers.SerializerMethodField()
     graduation_date = serializers.SerializerMethodField()
     phone_numbers = serializers.SerializerMethodField()
     addresses = serializers.SerializerMethodField()
-    
-    
 
-    class Meta:
-        model = Student
-        fields = "__all__" 
-        extra_fields = ["email","addresses", "phone_numbers"]
-        
-
-    def get_registration_type(self, obj):
-        return obj.registration_type.title
-
-    def get_field_of_study(self,obj):
-        return obj.field_of_study.title
-
-    
-    def get_graduation_date(self,obj):
-        if not obj.graduation_date:
-            return "نا مشخص"
-        return obj.graduation_date
-    
-    def get_addresses(self,obj):
-        all_addresses=Address.objects.filter(person = obj.person)
-        return [address.address for address in all_addresses]
-    
-    def get_emails(self, obj):
-        all_emails = Email.objects.filter(person_id=obj.person_id)
-        return [email.email for email in all_emails]
-    
-    
-    def get_phone_numbers(self, obj):
-        all_numbers = PhoneNumber.objects.filter(person_id=obj.person_id)
-        return [phone.number for phone in all_numbers]
-    
-        
-class StudentRequestSerializer(serializers.Serializer):
     class Meta:
         model = Student
         fields = "__all__"
+        extra_fields = ["email", "addresses", "phone_numbers"]
+
+    def get_registration_type(self, obj):
+        return obj.registration_type.title if obj.registration_type else None
+
+    def get_field_of_study(self, obj):
+        return obj.field_of_study.title if obj.field_of_study else None
+
+    def get_graduation_date(self, obj):
+        return obj.graduation_date if obj.graduation_date else "نامشخص"
+
+    def get_addresses(self, obj):
+        all_addresses = Address.objects.filter(person=obj.person)
+        return [address.address for address in all_addresses]
+
+    def get_emails(self, obj):
+        all_emails = Email.objects.filter(person_id=obj.person_id)
+        return [email.email for email in all_emails]
+
+    def get_phone_numbers(self, obj):
+        all_numbers = PhoneNumber.objects.filter(person_id=obj.person_id)
+        return [phone.number for phone in all_numbers]
+
+
+class StudentRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = "__all__"
-    
