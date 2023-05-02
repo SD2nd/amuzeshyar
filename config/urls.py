6d31961318acfd94cc00582796a525e2f62d856c
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2023-05-02 14:15:31 +0430

    new home view error fixed

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from amuzeshyar import views
+from .views import home
 
 urlpatterns = [
-    path('',views.home),
+    path('',home),
     path('admin/', admin.site.urls),
     path('edu/api/v1/', include("amuzeshyar.urls")),
     path('edu/api/schema/', SpectacularAPIView.as_view(), name='schema'),
