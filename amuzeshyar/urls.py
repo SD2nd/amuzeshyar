375b4fb826dfbf8f3179ad3cc475bb5d6271ad9f
Amir Iranparast <a.iranparast@gmail.com>
2023-05-02 13:39:06 +0330

    jwt auth utl added

--- amuzeshyar/urls.py
+++ amuzeshyar/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
 from amuzeshyar import apis as a
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 # config URL Configuration
 
 # The `urlpatterns` list routes URLs to views. For more information please see:
@@ -51,4 +55,8 @@ urlpatterns = [
     
     #ConstValue    
     path('constvalues/', a.ConstValue.as_view()),
+
+    # JWT Authenticaion
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
